1ef1f29f2eb66be806140f8dfc1cee39da59ce3d
Jayden Ramirez <jaydenr0311@gmail.com>
2025-03-09 18:03:32 -0400

    Work Day 3/9/2025
    - Deleted default input systems
    - Added minigameManager.cs for launcher linking utility BUT DID NOT YET complete
    - Added a player2 boat that will likely be deleted
    - Refactored boat movement script that may be replaced

--- Assets/BoatScript.cs
+++ Assets/BoatScript.cs
@@ -4,33 +4,55 @@ using UnityEngine.InputSystem;
 public class BoatScript : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float maxFallingSpeed = -10;
-    public float maxRisingSpeed = 10;
+    public float maxFallingSpeed = -10f;
+    public float maxRisingSpeed = 10f;
+    public float maxHorizontalSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocityY = maxFallingSpeed;
+        rb.linearVelocity = new Vector2(0, maxFallingSpeed);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame, Fixedupdate is called more sparingly
     void Update()
     {
-        /*Vertical Movement*/
+        if(Input.GetKeyUp(KeyCode.W))    {
+            rb.linearVelocityY = -3;
+        }
+        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))  {
+            rb.linearVelocityX = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        verticalMovement();
+        horizontalMovement();
+    }
+    void verticalMovement() {
         if(rb.linearVelocityY < maxFallingSpeed)    {
             rb.linearVelocityY = maxFallingSpeed;
         }
-        if(Input.GetKeyUp(KeyCode.Space)) {
-            rb.linearVelocityY = 0;
-        }
-        if(Input.GetKey(KeyCode.Space)) {
+        if(Input.GetKey(KeyCode.W)) {
             if(rb.linearVelocityY < maxRisingSpeed) {
                 rb.linearVelocityY += 1;
             }
         }
         else    {
             rb.linearVelocityY -= 1;
+        }
+    }
+    void horizontalMovement()   {
+        if(Input.GetKey(KeyCode.A)) {
+            if(rb.linearVelocityX > maxHorizontalSpeed * -1) {
+                rb.linearVelocityX -= 1;
+            }
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            if(rb.linearVelocityX < maxHorizontalSpeed) {
+                rb.linearVelocityX += 1;
+            }
         }
     }
 }
